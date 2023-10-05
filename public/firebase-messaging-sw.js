388ee1616fa9c9eb79c9ee9cc260b1b42c0a50b1
tohbay <tobechukwuobitube@gmail.com>
2023-10-05 10:20:01 +0100

    update .env file

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -6,13 +6,20 @@ importScripts("https://www.gstatic.com/firebasejs/7.9.1/firebase-messaging.js");
 // );
 
 firebase?.initializeApp({
-  apiKey: `${process.env.apiKey}`,
-  authDomain: `${process.env.authDomain}`,
-  projectId: `${process.env.projectId}`,
-  storageBucket: `${process.env.storageBucket}`,
-  messagingSenderId: `${process.env.messagingSenderId}`,
-  appId: `${process.env.appId}`,
-  measurementId: `${process.env.measurementId}`,
+  apiKey: "AIzaSyCJFA2se-7TQ6OfAm3tr7t_V_BoDVRK6cs",
+  authDomain: "pwablog-2d42d.firebaseapp.com",
+  projectId: "pwablog-2d42d",
+  storageBucket: "pwablog-2d42d.appspot.com",
+  messagingSenderId: "11936801963",
+  appId: "1:11936801963:web:97ca84f0e153a49c35c3ee",
+  measurementId: "G-GBT8WQ4MQ8",
+  // apiKey: `${process.env.apiKey}`,
+  // authDomain: `${process.env.authDomain}`,
+  // projectId: `${process.env.projectId}`,
+  // storageBucket: `${process.env.storageBucket}`,
+  // messagingSenderId: `${process.env.messagingSenderId}`,
+  // appId: `${process.env.appId}`,
+  // measurementId: `${process.env.measurementId}`,
 });
 
 const messaging = firebase.messaging();
--- src/webPush.js
+++ src/webPush.js
@@ -4,17 +4,23 @@ import localforage from "localforage";
 
 const firebaseCloudMessaging = {
   init: async () => {
-    console.log(process.env.apiKey);
     if (!firebase?.apps?.length) {
       // Initialize the Firebase app with the credentials
       firebase?.initializeApp({
-        apiKey: `${process.env.apiKey}`,
-        authDomain: `${process.env.authDomain}`,
-        projectId: `${process.env.projectId}`,
-        storageBucket: `${process.env.storageBucket}`,
-        messagingSenderId: `${process.env.messagingSenderId}`,
-        appId: `${process.env.appId}`,
-        measurementId: `${process.env.measurementId}`,
+        apiKey: "AIzaSyCJFA2se-7TQ6OfAm3tr7t_V_BoDVRK6cs",
+        authDomain: "pwablog-2d42d.firebaseapp.com",
+        projectId: "pwablog-2d42d",
+        storageBucket: "pwablog-2d42d.appspot.com",
+        messagingSenderId: "11936801963",
+        appId: "1:11936801963:web:97ca84f0e153a49c35c3ee",
+        measurementId: "G-GBT8WQ4MQ8",
+        // apiKey: `${process.env.apiKey}`,
+        // authDomain: `${process.env.authDomain}`,
+        // projectId: `${process.env.projectId}`,
+        // storageBucket: `${process.env.storageBucket}`,
+        // messagingSenderId: `${process.env.messagingSenderId}`,
+        // appId: `${process.env.appId}`,
+        // measurementId: `${process.env.measurementId}`,
       });
 
       try {
@@ -31,7 +37,10 @@ const firebaseCloudMessaging = {
         if (status && status === "granted") {
           // Get new token from Firebase
           const fcm_token = await messaging?.getToken({
-            vapidKey: `${process.env.vapidKey}`,
+            // vapidKey:
+            //   `${vapidKey}`,
+            vapidKey:
+              "BCAikdqYQnDT7_ERaLZTJR_Ly4E3ckUlZFn4dG01iJy8P_q43-DNwCb0UdMzpkFJm3Lr4XhHaH_GAO5Jz9730zQ",
           });
 
           console.log({ fcm_token });
