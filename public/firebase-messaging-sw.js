b4cdde11391a7b088b20c49598fd3a5e6deff391
tohbay <tobechukwuobitube@gmail.com>
2023-10-12 18:27:10 +0100

    update push notifications

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -8,20 +8,20 @@ importScripts("https://www.gstatic.com/firebasejs/7.9.1/firebase-messaging.js");
 // console.log(process?.env?.apiKey);
 
 firebase?.initializeApp({
-  // apiKey: "AIzaSyCJFA2se-7TQ6OfAm3tr7t_V_BoDVRK6cs",
-  // authDomain: "pwablog-2d42d.firebaseapp.com",
-  // projectId: "pwablog-2d42d",
-  // storageBucket: "pwablog-2d42d.appspot.com",
-  // messagingSenderId: "11936801963",
-  // appId: "1:11936801963:web:97ca84f0e153a49c35c3ee",
-  // measurementId: "G-GBT8WQ4MQ8",
-  apiKey: `${process?.env?.NEXT_PUBLIC_apiKey}`,
-  authDomain: `${process?.env?.NEXT_PUBLIC_authDomain}`,
-  projectId: `${process?.env?.NEXT_PUBLIC_projectId}`,
-  storageBucket: `${process?.env?.NEXT_PUBLIC_storageBucket}`,
-  messagingSenderId: `${process?.env?.NEXT_PUBLIC_messagingSenderId}`,
-  appId: `${process?.env?.NEXT_PUBLIC_appId}`,
-  measurementId: `${process?.env?.NEXT_PUBLIC_measurementId}`,
+  apiKey: "AIzaSyCJFA2se-7TQ6OfAm3tr7t_V_BoDVRK6cs",
+  authDomain: "pwablog-2d42d.firebaseapp.com",
+  projectId: "pwablog-2d42d",
+  storageBucket: "pwablog-2d42d.appspot.com",
+  messagingSenderId: "11936801963",
+  appId: "1:11936801963:web:97ca84f0e153a49c35c3ee",
+  measurementId: "G-GBT8WQ4MQ8",
+  // apiKey: `${process?.env?.NEXT_PUBLIC_apiKey}`,
+  // authDomain: `${process?.env?.NEXT_PUBLIC_authDomain}`,
+  // projectId: `${process?.env?.NEXT_PUBLIC_projectId}`,
+  // storageBucket: `${process?.env?.NEXT_PUBLIC_storageBucket}`,
+  // messagingSenderId: `${process?.env?.NEXT_PUBLIC_messagingSenderId}`,
+  // appId: `${process?.env?.NEXT_PUBLIC_appId}`,
+  // measurementId: `${process?.env?.NEXT_PUBLIC_measurementId}`,
 });
 
 const messaging = firebase.messaging();
